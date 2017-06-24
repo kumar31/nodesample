0b0f7a0eef0f3b6e481d95b6820ec286ff563bc2
kumar31 <kumarappan@smaatapps.com>
2017-06-24 17:33:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var routes = require('./server/routes');
+//var routes = require('./server/routes');
 
 //Render the client side application
-app.use(express.static(__dirname + '/client'));
+//app.use(express.static(__dirname + '/client'));
 
 //used convert the body to json
 app.use(bodyParser.json());
 
-app.use(routes);
+//app.use(routes);
 
 //Host the server
 var server = app.listen(5000, function () {
